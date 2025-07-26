ff3cf2bb12791fc0da675575e57e17f2ad004037
siddharthapalav@gmail.com <siddharthapalav@gmail.com>
2025-07-27 02:34:56 +0530

    todolist updated

--- 1-exercise-solutions/lesson-11/11x.js
+++ 1-exercise-solutions/lesson-11/11x.js
@@ -1,4 +1,5 @@
-const todoList = [{
+// When loading the page, load from localStorage.
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
   name: 'make dinner',
   dueDate: '2022-12-22'
 }, {
@@ -22,6 +23,9 @@ function renderTodoList() {
       <button onclick="
         todoList.splice(${i}, 1);
         renderTodoList();
+
+        // Whenever we update the todo list, save in localStorage.
+        saveToStorage();
       " class="delete-todo-button">Delete</button> 
     `;
     todoListHTML += html;
@@ -48,4 +52,11 @@ function addTodo() {
   inputElement.value = '';
 
   renderTodoList();
+
+  // Whenever we update the todo list, save in localStorage.
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
