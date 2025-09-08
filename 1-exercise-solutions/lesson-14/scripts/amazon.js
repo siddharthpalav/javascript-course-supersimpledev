a1d342fc4e69156a82d3ce16537053ea2bd6b22b
siddharthapalav@gmail.com <siddharthapalav@gmail.com>
2025-09-08 18:44:30 +0530

    changes added for lesson-14

---
+++ 1-exercise-solutions/lesson-14/scripts/amazon.js
@@ -0,0 +1,114 @@
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
+let productsHTML = "";
+
+products.forEach((product) => {
+  productsHTML += `<div class="product-container">
+            <div class="product-image-container">
+              <img
+                class="product-image"
+                src="${product.image}"
+              />
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img
+                class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png"
+              />
+              <div class="product-rating-count link-primary">${
+                product.rating.count
+              }</div>
+            </div>
+
+            <div class="product-price">$${formatCurrency(
+              product.priceCents
+            )}</div>
+
+            <div class="product-quantity-container">
+              <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart js-added-to-cart-${product.id}">
+              <img src="images/icons/checkmark.png" />
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+              product.id
+            }">Add to Cart</button>
+        </div>`;
+});
+
+document.querySelector(".js-product-grid").innerHTML = productsHTML;
+
+// We're going to use an object to save the timeout ids.
+// The reason we use an object is because each product
+// will have its own timeoutId. So an object lets us
+// save multiple timeout ids for different products.
+// For example:
+// {
+//   'product-id1': 2,
+//   'product-id2': 5,
+//   ...
+// }
+// (2 and 5 are ids that are returned when we call setTimeout).
+const addedMessageTimeouts = {};
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+function hideUnhideAddedMessage(productId) {
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  addedMessage.classList.add("added-to-cart-visible");
+
+  // Check if there's a previous timeout for this
+  // product. If there is, we should stop it.
+  const previousTimeoutId = addedMessageTimeouts[productId];
+  if (previousTimeoutId) {
+    clearTimeout(previousTimeoutId);
+  }
+
+  const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove("added-to-cart-visible");
+  }, 2000);
+
+  // Save the timeoutId for this product
+  // so we can stop it later if we need to.
+  addedMessageTimeouts[productId] = timeoutId;
+}
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const { productId } = button.dataset;
+
+    addToCart(productId);
+
+    updateCartQuantity();
+
+    hideUnhideAddedMessage(productId);
+  });
+});
---
+++ 1-exercise-solutions/lesson-14/scripts/checkout.js
@@ -0,0 +1,181 @@
+import {
+  calculateCartQuantity,
+  cart,
+  removeFromCart,
+  updateQuantity,
+} from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
+let matchingProduct,
+  cartSummaryHtml = "";
+
+cart.forEach((cartItem) => {
+  products.forEach((product) => {
+    if (product.id === cartItem.productId) {
+      matchingProduct = product;
+    }
+  });
+
+  cartSummaryHtml += `<div class="cart-item-container js-cart-item-container-${
+    matchingProduct.id
+  }">
+    <div class="delivery-date">Delivery date: Tuesday, June 21</div>
+
+    <div class="cart-item-details-grid">
+      <img
+        class="product-image"
+        src="${matchingProduct.image}"
+      />
+
+      <div class="cart-item-details">
+        <div class="product-name">
+          ${matchingProduct.name}
+        </div>
+        <div class="product-price">$${formatCurrency(
+          matchingProduct.priceCents
+        )}</div>
+        <div class="product-quantity">
+          <span> Quantity: <span class="quantity-label js-quantity-label-${
+            matchingProduct.id
+          }">${cartItem.quantity}</span> </span>
+          <span class="update-quantity-link link-primary js-udpate-link" data-product-id="${
+            matchingProduct.id
+          }">
+            Update
+          </span>
+          <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+          <span class="save-quantity-link link-primary js-save-link" data-product-id="${
+            matchingProduct.id
+          }">Save</span>
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+            matchingProduct.id
+          }">
+            Delete
+          </span>
+        </div>
+      </div>
+
+      <div class="delivery-options">
+        <div class="delivery-options-title">
+          Choose a delivery option:
+        </div>
+        <div class="delivery-option">
+          <input
+            type="radio"
+            checked
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}"
+          />
+          <div>
+            <div class="delivery-option-date">Tuesday, June 21</div>
+            <div class="delivery-option-price">FREE Shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input
+            type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}"
+          />
+          <div>
+            <div class="delivery-option-date">Wednesday, June 15</div>
+            <div class="delivery-option-price">$4.99 - Shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input
+            type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}"
+          />
+          <div>
+            <div class="delivery-option-date">Monday, June 13</div>
+            <div class="delivery-option-price">$9.99 - Shipping</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+});
+
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.remove();
+
+    updateCartQuantity();
+  });
+});
+
+document.querySelectorAll(".js-udpate-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    console.log(productId);
+
+    // Here's an example of a feature we can add: validation.
+    // Note: we need to move the quantity-related code up
+    // because if the new quantity is not valid, we should
+    // return early and NOT run the rest of the code. This
+    // technique is called an "early return".
+
+    const quantityInput = document.querySelector(
+      `.js-quantity-input-${productId}`
+    );
+    const newQuantity = Number(quantityInput.value);
+
+    if (newQuantity < 0 || newQuantity >= 1000) {
+      alert("Quantity must be at least 0 and less than 1000");
+      return;
+    }
+    updateQuantity(productId, newQuantity);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.classList.remove("is-editing-quantity");
+  });
+});
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector(
+    ".js-return-to-home-link"
+  ).innerHTML = `${cartQuantity} items`;
+}
+
+document.querySelectorAll(".js-save-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.classList.remove("is-editing-quantity");
+
+    const quantityInput = document.querySelector(
+      `.js-quantity-input-${productId}`
+    );
+    const newQuantity = Number(quantityInput.value);
+    updateQuantity(productId, newQuantity);
+
+    const quantityLabel = document.querySelector(
+      `.js-quantity-label-${productId}`
+    );
+
+    quantityLabel.innerHTML = newQuantity;
+
+    updateCartQuantity();
+  });
+});
+
+updateCartQuantity();
